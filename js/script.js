650e5090325f660622ed91101cf2bbe9b34e20f1
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-09 18:10:51 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Created by: Zaida Hammel
+// Created on: March 2021
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function shows "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
